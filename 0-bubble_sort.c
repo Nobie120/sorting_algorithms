d43b3bb4475a5a071b160ed3de2bcf439f307319
Nobie120 <n12507400@gmail.com>
2023-11-16 16:08:08 -0800

    the bubble sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,25 +4,25 @@
  * bubble_sort - algorathm for sorting
  * @array:The array to be sorted
  * @size:number of digits in the array
- * 
+ *
  */
 
 void bubble_sort(int *array, size_t size)
 {
-  size_t i, j;
-  int temp;
+	size_t i, j;
+	int temp;
 
-  for (i = 0;i < size - 1; i++)
-  {
-    for (j = 0; j < size - 1; j++)
-    {
-      if (array[j + 1] < array[j])
-      {
-        temp = array[j + 1];
-        array[j + 1] = array[j];
-        array[j] = temp;
-        print_array(array, size);
-      }
-    }
-  }
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j + 1] < array[j])
+			{
+				temp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 }
