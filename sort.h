d2cf987ba75503da961f66fb467b9dbc471a76bd
Nobie120 <n12507400@gmail.com>
2023-11-19 03:55:50 -0800

    The header file

--- sort.h
+++ sort.h
@@ -27,7 +27,7 @@ void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void quicksort(int *array, size_t size, ssize_t lb, ssize_t ub);
-size_t lumoto_partitiont (int *array, size_t size, ssize_t lb, ssize_t ub);
+size_t lumoto_partitiont(int *array, size_t size, ssize_t lb, ssize_t ub);
 void swap_2(int *array, size_t size, int *a, int *b);
 
 #endif
