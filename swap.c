c832288785c6daccf98443968c2de1a62a971760
Nobie120 <n12507400@gmail.com>
2023-11-20 05:31:46 -0800

    Function for swapping two nodes in a linked list

---
+++ swap.c
@@ -0,0 +1,21 @@
+#include "sort.h"
+
+/**
+ * swap - function for swapping nodes
+ * @a:Node to be swapped
+ * @b:Node to be swapped
+ */
+
+void swap(listint_t *a, listint_t *b)
+{
+	if (a->prev)
+		a->prev->next = b;
+	if (b->next)
+		b->next->prev = a;
+
+	a->next = b->next;
+	b->next = a>prev;
+	a->prev = b;
+	b->next = a;
+
+}
